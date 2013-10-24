a1d249d9b85961d4fdb81381ee3488ea4cef5ab9
Jay Hurst <jhurst@salesforce.com>
2013-10-24 15:49:05 -0700

    Remove validation

--- src/main/java/canvas/SignedRequest.java
+++ src/main/java/canvas/SignedRequest.java
@@ -72,7 +72,7 @@ public class SignedRequest {
             throw new SecurityException(String.format("Error [%s] deserializing JSON to Object [%s]", e.getMessage(), CanvasRequest.class.getName()), e);
         }
 
-        verify(secret, algorithm, encodedEnvelope, encodedSig);
+        //verify(secret, algorithm, encodedEnvelope, encodedSig);
 
         // If we got this far, then the request was not tampered with.
         // return the request as a Java object
@@ -104,7 +104,7 @@ public class SignedRequest {
                     typeRef.getClass().getName()), e);
         }
 
-        verify(secret, algorithm, encodedEnvelope, encodedSig);
+        //verify(secret, algorithm, encodedEnvelope, encodedSig);
 
         // If we got this far, then the request was not tampered with.
         // return the request as a JSON string.
